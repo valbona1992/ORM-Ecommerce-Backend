a8813ecae35d8b6a84a839d1b4acf4629d914953
valbona1992 <valbona12@gmail.com>
2021-09-10 17:07:40 -0500

    update API category routes file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
+    }
+  })
+  .then
 });
 
 router.get('/:id', (req, res) => {
