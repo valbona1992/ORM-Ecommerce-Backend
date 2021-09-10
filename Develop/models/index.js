793f30aadc4ed04c4cd5adc9e2fa3a3a93cd7411
valbona1992 <valbona12@gmail.com>
2021-09-10 18:22:39 -0500

    add columns in model js files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Product.hasMany(Product, {
+    foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Product.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
